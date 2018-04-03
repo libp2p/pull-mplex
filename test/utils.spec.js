117adbb775a1fb687cee19d3e8880a99e0101495
Dmitriy Ryajov <dryajov@gmail.com>
2018-04-03 16:00:12 -0600

    feat: correct encoding and channel closing

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -65,4 +65,25 @@ describe('utils', () => {
     )
   })
 
+  it('encodes zero length body msg', () => {
+    pull(
+      pull.values([[17, 0]]),
+      utils.encode(),
+      pull.collect((err, data) => {
+        expect(err).to.not.exist()
+        expect(data[0]).to.be.eql(Buffer.from('880100', 'hex'))
+      })
+    )
+  })
+
+  it('decodes zero length body msg', () => {
+    pull(
+      pull.values([Buffer.from('880100', 'hex')]),
+      utils.decode(),
+      pull.collect((err, data) => {
+        expect(err).to.not.exist()
+        expect(data[0]).to.be.eql({ id: 17, type: 0, data: Buffer.alloc(0) })
+      })
+    )
+  })
 })
