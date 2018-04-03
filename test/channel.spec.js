e9f991716f09f19a3e9de674a84338d6bff419a1
Dmitriy Ryajov <dryajov@gmail.com>
2018-04-02 22:57:45 -0600

    feat: improve performance

--- test/channel.spec.js
+++ test/channel.spec.js
@@ -5,6 +5,7 @@
 const chai = require('chai')
 const dirtyChai = require('dirty-chai')
 const expect = chai.expect
+chai.use(require('chai-checkmark'))
 chai.use(dirtyChai)
 
 const pull = require('pull-stream')
@@ -12,18 +13,28 @@ const pair = require('pull-pair/duplex')
 const pushable = require('pull-pushable')
 const abortable = require('pull-abortable')
 
-const Mplex = require('../src')
+const Plex = require('../src')
 const utils = require('../src/utils')
 const consts = require('../src/consts')
 
 const series = require('async/series')
 
+function closeAndWait (stream) {
+  pull(
+    pull.empty(),
+    stream,
+    pull.onEnd((err) => {
+      expect(err).to.not.exist.mark()
+    })
+  )
+}
+
 describe('channel', () => {
-  it('initiator should be able to send data between two multiplexers', (done) => {
+  it('initiator should be able to send data', (done) => {
     const p = pair()
 
-    const plex1 = new Mplex(true)
-    const plex2 = new Mplex(false)
+    const plex1 = new Plex(true)
+    const plex2 = new Plex(false)
 
     pull(plex1, p[0], plex1)
     pull(plex2, p[1], plex2)
@@ -39,18 +50,18 @@ describe('channel', () => {
       )
     })
 
-    const stream = plex1._newStream(plex1.nextChanId(true), true, 'stream 1')
+    const stream = plex1.createStream('stream 1')
     pull(
       pull.values([Buffer.from('hello from plex1!!')]),
       stream
     )
   })
 
-  it('receiver should be able to send data between two multiplexers', (done) => {
+  it('receiver should be able to send data', (done) => {
     const p = pair()
 
-    const plex1 = new Mplex(true)
-    const plex2 = new Mplex(false)
+    const plex1 = new Plex(true)
+    const plex2 = new Plex(false)
 
     pull(plex1, p[0], plex1)
     pull(plex2, p[1], plex2)
@@ -73,11 +84,11 @@ describe('channel', () => {
     })
   })
 
-  it('stream can be piped with itself (echo)', (done) => {
+  it('stream can be piped to itself (echo)', (done) => {
     const p = pair()
 
-    const plex1 = new Mplex(true)
-    const plex2 = new Mplex(false)
+    const plex1 = new Plex(true)
+    const plex2 = new Plex(false)
 
     pull(plex1, p[0], plex1)
     pull(plex2, p[1], plex2)
@@ -102,46 +113,11 @@ describe('channel', () => {
     )
   })
 
-  it('sending close msg closes stream', (done) => {
-    const plex = new Mplex(true)
-
-    plex.on('stream', (stream) => {
-      pull(
-        stream,
-        pull.collect((err, data) => {
-          expect(err).to.not.exist()
-          expect(data[0]).to.deep.eql(Buffer.from('hellooooooooooooo'))
-          done()
-        })
-      )
-    })
-
-    series([
-      (cb) => utils.encodeMsg(3,
-        consts.type.NEW,
-        Buffer.from('chan1'), cb),
-      (cb) => utils.encodeMsg(3,
-        consts.type.IN_MESSAGE,
-        Buffer.from('hellooooooooooooo'),
-        cb),
-      (cb) => utils.encodeMsg(3,
-        consts.type.IN_CLOSE,
-        Buffer.from([]),
-        cb)
-    ], (err, msgs) => {
-      expect(err).to.not.exist()
-      pull(
-        pull.values(msgs),
-        plex
-      )
-    })
-  })
-
-  it('closing sender closes stream for writting, but allows reading from it', (done) => {
+  it('closing sender closes stream for writting, but allows reading', (done) => {
     const p = pair()
 
-    const plex1 = new Mplex(true)
-    const plex2 = new Mplex(false)
+    const plex1 = new Plex(true)
+    const plex2 = new Plex(false)
 
     pull(plex1, p[0], plex1)
     pull(plex2, p[1], plex2)
@@ -150,7 +126,6 @@ describe('channel', () => {
     const rcvrSrc = pushable()
 
     plex2.on('stream', (receiver) => {
-
       pull(
         rcvrSrc,
         receiver
@@ -161,26 +136,24 @@ describe('channel', () => {
     })
 
     const sender = plex1.createStream()
-    sender.openChan(() => {
-      sndrSrc.end()
-
-      pull(
-        sndrSrc,
-        sender,
-        pull.collect((err, data) => {
-          expect(err).to.not.exist()
-          expect(data[0].toString()).to.be.eql('Here ya go!')
-          done()
-        })
-      )
-    })
+    sender.openChan()
+    sndrSrc.end()
+    pull(
+      sndrSrc,
+      sender,
+      pull.collect((err, data) => {
+        expect(err).to.not.exist()
+        expect(data[0].toString()).to.be.eql('Here ya go!')
+        done()
+      })
+    )
   })
 
-  it('closing receiver closes stream for writting, but allows reading from it', (done) => {
+  it('closing receiver closes stream for writting, but allows reading', (done) => {
     const p = pair()
 
-    const plex1 = new Mplex(true)
-    const plex2 = new Mplex(false)
+    const plex1 = new Plex(true)
+    const plex2 = new Plex(false)
 
     pull(plex1, p[0], plex1)
     pull(plex2, p[1], plex2)
@@ -203,22 +176,21 @@ describe('channel', () => {
     })
 
     const sender = plex1.createStream()
-    sender.openChan(() => {
-      pull(
-        sndrSrc,
-        sender
-      )
+    sender.openChan()
+    pull(
+      sndrSrc,
+      sender
+    )
 
-      sndrSrc.push('Here ya go!') // should be able to write to closed chan
-      sndrSrc.end()
-    })
+    sndrSrc.push('Here ya go!') // should be able to write to closed chan
+    sndrSrc.end()
   })
 
   it('closed sender should allow receiver to flush data', (done) => {
     const p = pair()
 
-    const plex1 = new Mplex(true)
-    const plex2 = new Mplex(false)
+    const plex1 = new Plex(true)
+    const plex2 = new Plex(false)
 
     pull(plex1, p[0], plex1)
     pull(plex2, p[1], plex2)
@@ -227,7 +199,6 @@ describe('channel', () => {
     const rcvrSrc = pushable()
 
     plex2.on('stream', (receiver) => {
-
       pull(
         rcvrSrc,
         receiver,
@@ -240,23 +211,21 @@ describe('channel', () => {
     })
 
     const sender = plex1.createStream()
-    sender.openChan((err) => {
-      expect(err).to.not.exist()
-      sndrSrc.push('hello from sender!')
-      sndrSrc.end()
+    sender.openChan()
+    sndrSrc.push('hello from sender!')
+    sndrSrc.end()
 
-      pull(
-        sndrSrc,
-        sender
-      )
-    })
+    pull(
+      sndrSrc,
+      sender
+    )
   })
 
   it('should reset channels', (done) => {
     const p = pair()
 
-    const plex1 = new Mplex(true)
-    const plex2 = new Mplex(false)
+    const plex1 = new Plex(true)
+    const plex2 = new Plex(false)
 
     pull(plex1, p[0], plex1)
     pull(plex2, p[1], plex2)
@@ -277,13 +246,42 @@ describe('channel', () => {
     const sndrSrc = pushable()
     const sender = plex1.createStream()
     const aborter = abortable()
-    sender.openChan((err) => {
-      expect(err).to.not.exist()
-      pull(
-        sndrSrc,
-        aborter,
-        sender
-      )
+    sender.openChan()
+    pull(
+      sndrSrc,
+      aborter,
+      sender
+    )
+  })
+
+  it('open a stream on both sides', (done) => {
+    const p = pair()
+
+    const dialer = new Plex(true)
+    const listener = new Plex(false)
+
+    pull(dialer, p[0], dialer)
+    pull(listener, p[1], listener)
+
+    expect(6).check(done)
+
+    dialer.on('stream', (stream) => {
+      expect(stream).to.exist.mark()
+      closeAndWait(stream)
     })
+
+    const listenerConn = listener.createStream('listener')
+    listenerConn.openChan()
+
+    listener.on('stream', (stream) => {
+      expect(stream).to.exist.mark()
+      closeAndWait(stream)
+    })
+
+    const dialerConn = dialer.createStream('dialer')
+    dialerConn.openChan()
+
+    closeAndWait(dialerConn)
+    closeAndWait(listenerConn)
   })
 })
