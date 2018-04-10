44c4c4ea320e83d01b92dbb444e6ce56634932d2
Dmitriy Ryajov <dryajov@gmail.com>
2018-04-10 15:10:35 -0600

    feat: validation and coverage

--- test/plex.spec.js
+++ test/plex.spec.js
@@ -10,11 +10,13 @@ chai.use(dirtyChai)
 
 const pull = require('pull-stream')
 const pair = require('pull-pair/duplex')
+const abortable = require('pull-abortable')
 
+const coder = require('../src/coder')
 const Plex = require('../src')
 
 describe('plex', () => {
-  it.skip(`reset should close both ends`, (done) => {
+  it(`reset should close both ends`, (done) => {
     const p = pair()
 
     const plex1 = new Plex(true)
@@ -39,6 +41,116 @@ describe('plex', () => {
       expect().mark()
     })
     plex1.reset()
+  })
+
+  it(`closing stream should close all channels`, (done) => {
+    const aborter = abortable()
+    const plex1 = new Plex()
+
+    plex1.on('error', (() => {}))
+
+    pull(plex1, aborter)
+
+    expect(2).check(done)
+
+    const stream1 = plex1.createStream()
+    stream1.on('error', (() => {}))
+
+    const stream2 = plex1.createStream()
+    stream2.on('error', (() => {}))
+    pull(
+      stream1,
+      pull.onEnd((err) => {
+        expect(err).to.exist().mark()
+      })
+    )
+
+    pull(
+      stream2,
+      pull.onEnd((err) => {
+        expect(err).to.exist().mark()
+      })
+    )
+
+    aborter.abort()
+  })
+
+  it(`error should propagate to all channels`, (done) => {
+    const aborter = abortable()
+    const plex1 = new Plex()
+
+    plex1.on('error', (() => {}))
+
+    pull(plex1, aborter)
+
+    expect(2).check(done)
+
+    const stream1 = plex1.createStream()
+    stream1.on('error', (() => {}))
+
+    const stream2 = plex1.createStream()
+    stream2.on('error', (() => {}))
+
+    pull(
+      stream1,
+      pull.onEnd((err) => {
+        expect(err.message).to.eql('nasty error').mark()
+      })
+    )
+
+    pull(
+      stream2,
+      pull.onEnd((err) => {
+        expect(err.message).to.eql('nasty error').mark()
+      })
+    )
+
+    aborter.abort(new Error('nasty error'))
+  })
+
+  it(`should fail if max number of channels exceeded`, (done) => {
+    const plex1 = new Plex({
+      maxChannels: 10,
+      lazy: true
+    })
+
+    plex1.on('error', (err) => {
+      expect(err.message).to.eql('max channels exceeded')
+      done()
+    })
+
+    for (let i = 0; i < 11; i++) {
+      plex1.createStream()
+    }
+  })
+
+  it(`should restrict message size`, (done) => {
+    const plex = new Plex()
+
+    plex.on('error', function (err) {
+      expect(err.message).to.equal('message too large!')
+      done()
+    })
+
+    pull(
+      pull.values([Array(1048576 + 1).join('\xff')]), // 1mb
+      plex
+    )
+  })
+
+  it(`should validate message`, (done) => {
+    const plex = new Plex()
+
+    plex.on('error', function (err) {
+      expect(err.message).to.equal('Invalid message type')
+      done()
+    })
+
+    pull(
+      pull.values([[1, 7]]),
+      coder.encode(), // invalid message type
+      plex
+    )
   })
 
   describe(`check id`, () => [true, false].forEach((initiator) => {
